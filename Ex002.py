44a7e1a8cf8cb80e6595ad17dbe8b63eef7cc5a1
MarceloVasselai <mvasselai@gmail.com>
2021-03-26 18:10:36 -0300

    Novo Exerc2

---
+++ Ex002.py
@@ -0,0 +1,5 @@
+nome = input('Digite o seu nome: ')
+idade = input('Digite a sua idade: ')
+time = input('Digite o seu time: ')
+
+print('Olá  {} é um prazer te conhecer, a sua idade é {} e o seu time é o {} !'.format(nome, idade, time))
