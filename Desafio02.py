6c87c9cc34b4515e06d2274a2b14ff9f7308e96a
MarceloVasselai <mvasselai@gmail.com>
2021-03-26 09:19:59 -0300

    primeiro commit

---
+++ Desafio02.py
@@ -0,0 +1,6 @@
+dia = input('Qual é o dia ?: ')
+mes = input('Qual é o mês ?: ')
+ano = input('Qual é o ano ?: ')
+
+print('Você nasceu no dia: ' + dia + '/' + mes + '/' + ano + '. Correto ?')
+
---
+++ Desafio03.py
@@ -0,0 +1,6 @@
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+tot = int(num1 + num2)
+
+print(num1, ' + ', num2, ' = ', tot)
+
