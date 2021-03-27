c49fd463f86014e6b1135ad384549330dc23e566
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 18:32:33 -0300

    Lica

---
+++ Ex006.py
@@ -0,0 +1,6 @@
+numero = int(input('Qual o número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = float(numero) ** 0.5
+print (' O número digitado foi: {}\n o dobro é: {}\n dobro o tripo é {}\n e a raiz quadrada é {} '.format(numero, dobro,triplo,raiz))
+
