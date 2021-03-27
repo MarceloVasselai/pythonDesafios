19f3f1062728055a05ed64d5c739a0a0a3a0da1f
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 19:45:30 -0300

    Transformar em Dólar

---
+++ Ex010.py
@@ -0,0 +1,3 @@
+real = float(input('Quantos reais você tem em sua Carteira: R$ '))
+print ('Transformando para Dólar, você tem exatamente U$ {:.2f} isso no dia do curso'.format(real/3.27))
+print ('Pois nos dias de hoje, você tem exatamente U$ {:.2f}'.format(real/5.76))
