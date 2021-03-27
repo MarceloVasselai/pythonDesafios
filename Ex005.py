f788a84d3a1b22ea35c42881c8d0d5779fb99b54
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 17:51:29 -0300

    Lição 1

---
+++ Ex005.py
@@ -0,0 +1,9 @@
+numero = int(input('Digite o número: '))
+antecessor = numero -1
+posterior = numero +1
+
+print ('O número digitado foi {} o seu antecessor é {} e o seu posterior é: {}'.format(numero,antecessor, posterior))
+
+
+
+
