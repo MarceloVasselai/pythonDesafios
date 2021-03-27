e17aac662f8075090fb6b5786e2d12cf5c75fc82
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 19:01:37 -0300

    Data de Aniversário

--- Desafio02.py
+++ Desafio02.py
@@ -2,5 +2,5 @@ dia = input('Qual é o dia ?: ')
 mes = input('Qual é o mês ?: ')
 ano = input('Qual é o ano ?: ')
 
-print('Você nasceu no dia: ' + dia + '/' + mes + '/' + ano + '. Correto ?')
+print('Você nasceu no dia: ' +dia+ '/' +mes+ '/' +ano+ '. Correto ?')
 
