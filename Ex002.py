dea3461ba500895b898292139e9c7ff64452e153
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 18:57:44 -0300

    Nome, Idade e Time

--- Ex002.py
+++ Ex002.py
@@ -2,4 +2,4 @@ nome = input('Digite o seu nome: ')
 idade = input('Digite a sua idade: ')
 time = input('Digite o seu time: ')
 
-print('Olá  {} é um prazer te conhecer, a sua idade é {} e o seu time é o {} !'.format(nome, idade, time))
+print('Olá  {} é um prazer te conhecer, a sua idade é {} e o seu time é o {} !'.format(nome,idade,time))
