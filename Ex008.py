fd90fd591b15404bf00e2f9cbcbd2603cdb7987a
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 19:11:02 -0300

    Metros em Centímetros em Milímetros

--- Ex008.py
+++ Ex008.py
@@ -1,2 +1,2 @@
 metro = int(input('Digite o valor em Metros: '))
-print('O valor em metros é {}, em centímetos é: {} e em milimetros é {}: '.format(metro,metro*100,metro*1000))
+print('O valor em metros é {} mt, em centímetos é {} cm e em milimetros é {} mm: '.format(metro,metro*100,metro*1000))
