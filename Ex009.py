05a81dfc9533da0fe68a0c8f293c54d078fd5f1c
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 19:32:06 -0300

    Tabuada

---
+++ Ex009.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+print ('A tabuada do {} é: \n{} * 1 = {}\n{} * 2 = {}\n{} * 3 = {}\n{} * 4 = {}\n{} * 5 = {}\n'
+       '{} * 6 = {}\n{} * 7 = {}\n{} * 8 = {}\n{} * 9 = {}\n{} * 10 = {}\n'
+       .format(numero,numero,numero*1,numero,numero*2,numero,numero*3,numero,numero*4,numero,numero*5,numero,
+               numero*6,numero,numero*7,numero,numero*8,numero,numero*9,numero,numero*10))
