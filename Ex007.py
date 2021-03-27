1ca429de92eb4cb720aaa9d2b6d1f7e4ac2ce41b
MarceloVasselai <mvasselai@gmail.com>
2021-03-27 18:45:00 -0300

    Média

---
+++ Ex007.py
@@ -0,0 +1,4 @@
+notaM = int(input('Qual a Nota de Matemática: '))
+notaP = int(input('Qual a Nota de Português: '))
+
+print ('A nota de Matemática é {} a nota de português é {} e a média é {}'.format(notaM,notaP,(notaM+notaP)/2) )
