b7d097d9736eea2e8caf8897fc5ee68d49b6933b
MarceloVasselai <mvasselai@gmail.com>
2021-03-29 11:20:20 -0300

    Cáculo de Área x Litros de Tinta

---
+++ Ex011.py
@@ -0,0 +1,4 @@
+largura = float(input ('Largura da Parede: '))
+altura = float(input ('Altura da Parede: '))
+
+print ('Área de {} m² sendo necessário {} litros de tinta  '.format(largura*altura,(largura*altura)/2))
