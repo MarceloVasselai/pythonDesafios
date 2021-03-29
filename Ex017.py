02d2d8427744a43ede2f4b9bf756a6dd49be00c7
MarceloVasselai <mvasselai@gmail.com>
2021-03-29 17:17:04 -0300

    Hipotenusa

--- Ex017.py
+++ Ex017.py
@@ -1,7 +1,18 @@
-
 import math
 
-sen_a = float(input('Informe o Seno do A = '))
+#tradicional
 cat_o = float(input('Informe o Cateto Oposto = '))
+cat_a = float(input('Informe o Cateto Adjacente = '))
+hipot = (cat_o ** 2 + cat_a ** 2)  ** (1/2)
+print('O valor do Cateto Oposto é {:.2f}, do Cateto Adjacente é {:.2f}, logo a Hipotenusa será {:.2f}'.format(cat_o,cat_a,hipot))
 
-print('O valor do Seno é {}, do Cateto é {}, logo a Hipotenusa será {}'.format(sen_a,cat_o,cat_o/sen_a))
+print('')
+print('Agora usando o Import Math')
+print('2')
+
+#import math
+cat_o = float(input('Informe o Cateto Oposto = '))
+cat_a = float(input('Informe o Cateto Adjacente = '))
+hipot = math.hypot(cat_o,cat_a)
+print('O valor do Cateto Oposto é {:.2f}, do Cateto Adjacente é {:.2f}, logo a Hipotenusa será {:.2f}'.format(cat_o,cat_a,hipot))
+
