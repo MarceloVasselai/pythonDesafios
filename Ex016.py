173026b6c407d9dfe8f9821ee4672ae6bfcf6d78
MarceloVasselai <mvasselai@gmail.com>
2021-03-29 16:21:48 -0300

    Arredondamento

---
+++ Ex016.py
@@ -0,0 +1,7 @@
+import math
+
+x = int(input('Qual a Raiz de: '))
+print ('a Raíz Quadrada de {:.2f} é {:.2f}'.format(x,math.sqrt(x)))
+
+y = float(input('Digite o número:'))
+print('Vamos arredondar {} para {}'.format(y,math.trunc(y)))
