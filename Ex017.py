f92debfc974b95e4faa234210ad12adbae975a7c
MarceloVasselai <mvasselai@gmail.com>
2021-03-29 16:26:04 -0300

    Arredondamento

---
+++ Ex017.py
@@ -0,0 +1,6 @@
+import math
+
+sen_a = float(input('Informe o Seno do A = '))
+cat_o = float(input('Informe o Cateto Oposto = '))
+
+print('O valor do Seno é {}, do Cateto é {}, logo a Hipotenusa será {}'.format(sen_a,cat_o,cat_o/sen_a))
