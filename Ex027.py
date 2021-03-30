8af5263a6f05ddac1d0713e055011c0b251855a7
MarceloVasselai <mvasselai@gmail.com>
2021-03-30 14:25:30 -0300

    Manipulando Texto VI

--- Ex027.py
+++ Ex027.py
@@ -1,9 +1,11 @@
-nome = input('Digite um Nome: ')
+nome = str(input('Digite um Nome: ')).strip()
 primeiro_nome = nome.split()
 ultimo_nome = nome.rsplit()
 pos_ult_nome = int(nome.count(' '))
 
-
 print('O nome digitado foi: {}'.format(nome))
 print('O primeiro nome foi: {} '.format(primeiro_nome[0]))
 print('O último nome foi: {} '.format(ultimo_nome[pos_ult_nome]))
+
+print('Outra opção')
+print('O último nome foi: {} '.format(primeiro_nome[len(primeiro_nome)-1]))
