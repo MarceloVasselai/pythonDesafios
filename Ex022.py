7dd95f9e2fc0670af33ad18bc0df011dc4de3ef9
MarceloVasselai <mvasselai@gmail.com>
2021-03-30 09:54:19 -0300

    Manipulando Texto

--- Ex022.py
+++ Ex022.py
@@ -5,8 +5,8 @@ primeiro_nome = nome.split()
 
 print('Nome em maiúsculo: ' + nome.upper())
 print('Nome em minúsculo: ' + nome.lower())
-print('Total de Letras sem Espaços: ' + str(tot_sem_espacos))
-print('Primeiro Nome: {}'.format(len(primeiro_nome[0])))
+print('Total de Letras no nome sem Espaços: ' + str(tot_sem_espacos))
+print('Total de Letras no primeiro nome: {}'.format(len(primeiro_nome[0])))
 
 
 
