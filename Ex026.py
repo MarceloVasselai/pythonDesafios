2573ba1c9ea65327fd4dd1e7a6b5701caf14603e
MarceloVasselai <mvasselai@gmail.com>
2021-03-30 14:15:59 -0300

    Manipulando Texto V

--- Ex026.py
+++ Ex026.py
@@ -1,8 +1,5 @@
 frase = input('Digite uma frase: ')
-tot_a = frase.count('A')
-prim_vez = frase.find('A')
-ult_vez = frase.rfind('A')
 
-print('A letra A aparece {} vezes'.format(tot_a))
-print('A letra A aparece pela primeira vez na casa {} '.format(prim_vez))
-print('A letra A aparece pela última vez na casa {} '.format(ult_vez))
+print('A letra A aparece {} vezes'.format(frase.count('A')))
+print('A letra A aparece pela primeira vez na casa {} '.format(frase.find('A')))
+print('A letra A aparece pela última vez na casa {} '.format(frase.rfind('A')))
