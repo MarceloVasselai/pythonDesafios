6a1922343d30db215cc002aa7822a4d561a9ff29
MarceloVasselai <mvasselai@gmail.com>
2021-03-30 13:57:48 -0300

    Manipulando Texto I

--- Ex022.py
+++ Ex022.py
@@ -1,4 +1,4 @@
-nome = input('Digite o seu nome: ')
+nome = str(input('Digite o seu nome: ')).strip()
 nome_s_espaço = nome.replace(' ','')
 tot_sem_espacos = int(len(nome_s_espaço))
 primeiro_nome = nome.split()
@@ -11,6 +11,15 @@ print('Nome em minúsculo: ' + nome.lower())
 print('Total de Letras no nome sem Espaços I : ' + str(tot_sem_espacos))
 print('Total de Letras no nome sem Espaços II: ' + str(sem_espaco_II))
 print('Total de Letras no primeiro nome: {}'.format(len(primeiro_nome[0])))
+
+print('CORREÇÃO')
+print('Seu nome tem ao todo {} Letras:  '.format(len(nome)- nome.count(' ')))
+print('Seu primeiro nome tem {} Letras: '.format(nome.find(' ')))
+
+separa = nome.split()
+print('Seu primeiro nome tem {} e ele tem {} Letras: '.format(separa[0],len(separa[0])))
+
+
 
 
 
