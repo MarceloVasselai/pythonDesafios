b88667ad49837c8f2b22f1365f5906e78492459f
MarceloVasselai <mvasselai@gmail.com>
2021-03-30 11:05:19 -0300

    Manipulando Texto II

--- Ex023.py
+++ Ex023.py
@@ -1,5 +1,12 @@
 numero = input('Digite um número de 0 a 9999: ')
+numero =  str(numero).zfill(4)
 
-unidade = numero[3:4]
+unidade  = numero[3:4]
+dezena  = numero[2:3]
+centena = numero[1:2]
+milhare = numero[0:1]
 
 print('Unidade do número é: {}'.format(unidade))
+print('Dezena do número é: {}'.format(dezena))
+print('Centena do número é: {}'.format(centena))
+print('Milhar do número é: {}'.format(milhare))
