990351f6af160f4c385760eb6dbfac99521a519e
MarceloVasselai <mvasselai@gmail.com>
2021-03-30 11:10:32 -0300

    Manipulando Texto III

--- Ex024.py
+++ Ex024.py
@@ -2,7 +2,7 @@ cidade = input('Digite o nome da cidade: ')
 cidade = cidade.split()
 cidade = cidade[0]
 
-print('A cidade digitada foi {} '.format(cidade))
+print('A cidade digitada foi {}'.format(cidade))
 print('Santo' in cidade)
 
 
