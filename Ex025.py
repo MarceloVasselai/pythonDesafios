568e83454069dd8019f7f5eb806952b498a62866
MarceloVasselai <mvasselai@gmail.com>
2021-03-30 14:12:34 -0300

    Manipulando Texto IV

--- Ex025.py
+++ Ex025.py
@@ -3,3 +3,9 @@ nome = input('Digite o nome : ')
 print('A nome digitado foi {}'.format(nome))
 print('SILVA' in nome)
 
+print('Outra opção')
+print('Seu nome tem Silva? {}'.format(nome[:5] == 'Silva'))
+
+
+
+
