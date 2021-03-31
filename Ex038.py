9e7f299476d1aff2384fd6ad14971e870dd86b31
MarceloVasselai <mvasselai@gmail.com>
2021-03-31 18:28:14 -0300

    Condição IF - Comparação entre números

---
+++ Ex038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Informe o primeiro número: '))
+n2 = int(input('Informe o segundo número: '))
+
+if n1>n2:
+    print('O primeiro valor é maior')
+elif n1<n2:
+    print('O segundo valor é maior')
+elif n1==n2:
+    print('Não existe valor maior ou menor, são iguais')
