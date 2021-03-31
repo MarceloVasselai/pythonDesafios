e85371daacc3f2c5fbfd7c12dd3718320559fe36
MarceloVasselai <mvasselai@gmail.com>
2021-03-31 09:56:13 -0300

    Condição IF - O maior e menor entre 3 números

--- Ex033.py
+++ Ex033.py
@@ -2,12 +2,12 @@ n1 = int(input('Informe o primeiro número: '))
 n2 = int(input('Informe o segundo número: '))
 n3 = int(input('Informe o terceiro número: '))
 
-if n1 >= n2 and n1 >= n3:
-    print('O maior número é: {}'.format(n1))
-elif n2 >= n1 and n2 >= n3:
-    print('O maior número é: {}'.format(n2))
-elif n3 >= n1 and n3 >= n2:
-    print('O maior número é: {}'.format(n3))
+lista = [n1,n2,n3]
+
+print('Mínimo: {}'.format(min(lista)))
+print('Máximo: {}'.format(max(lista)))
+
+print('** OU **')
 
 if n1 <= n2 and n1 <= n3:
     print('O menor número é: {}'.format(n1))
@@ -16,3 +16,9 @@ elif n2 <= n1 and n2 <= n3:
 elif n3 <= n1 and n3 <= n2:
     print('O menor número é: {}'.format(n3))
 
+if n1 >= n2 and n1 >= n3:
+    print('O maior número é: {}'.format(n1))
+elif n2 >= n1 and n2 >= n3:
+    print('O maior número é: {}'.format(n2))
+elif n3 >= n1 and n3 >= n2:
+    print('O maior número é: {}'.format(n3))
