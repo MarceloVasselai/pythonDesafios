6cef6ade54886f64507b49eabcf05e48ce3f724c
MarceloVasselai <mvasselai@gmail.com>
2021-03-31 09:25:00 -0300

    Condição IF - Velocidade máxima com Multa

---
+++ Ex031.py
@@ -0,0 +1,7 @@
+distancia = float(input('Qual a distância da viagem: '))
+
+if distancia <= 200:
+    print('O valor passagem até 200 Km será: {}'.format(distancia*0.50))
+else:
+    print('O valor passagem com mais de 200 Km será: R$ {}'.format(distancia*0.45))
+    print('Blz?')
