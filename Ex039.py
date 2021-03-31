514bb683073eebdc6765dc5be30f979d0a37635c
MarceloVasselai <mvasselai@gmail.com>
2021-03-31 18:37:12 -0300

    Condição IF - Ano de Alistamento

---
+++ Ex039.py
@@ -0,0 +1,8 @@
+Ano_Nascimento = int(input('Informe o ano de nascimento: '))
+
+if 2021 - Ano_Nascimento <= 17:
+    print('Ainda vai se alistar')
+elif 2021 - Ano_Nascimento >17 and 2021 - Ano_Nascimento <= 18:
+    print('Hora de se alistar')
+elif 2021 - Ano_Nascimento > 18:
+    print('passou da hora')
