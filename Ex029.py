20b077e60fb82aef84b34cc590bc016f08f0c2dd
MarceloVasselai <mvasselai@gmail.com>
2021-03-31 07:46:35 -0300

    Velocidade Máxima com Multa  - Condição IF

---
+++ Ex029.py
@@ -0,0 +1,8 @@
+velocidade = int(input('Qual a velocidade do carro: '))
+excedeu =  velocidade - 80
+
+if velocidade <= 80:
+    print('Não há multa')
+else:
+    print('Velocidade de {} Km/h, ultrapassou em {} Km e a multa será de R$ {:.2f}'.format(velocidade,excedeu,excedeu * 7))
+    print('Blz?')
