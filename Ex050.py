7c367b715e9d2301afb8a3b6b894cfb70c362e21
MarceloVasselai <mvasselai@gmail.com>
2021-04-06 11:31:39 -0300

    Laços For - Somar apenas os números pares

--- Ex050.py
+++ Ex050.py
@@ -1,16 +1,5 @@
-n1=int(input('informe o primeiro número: '))
-n2=int(input('informe o segundo  número: '))
-n3=int(input('informe o terceiro número: '))
-n4=int(input('informe o quarto   número: '))
-n5=int(input('informe o quinto   número: '))
-n6=int(input('informe o sexto    número: '))
 tot=0
-
-if (n1 % 2) == 0: tot += n1
-if (n2 % 2) == 0: tot += n2
-if (n3 % 2) == 0: tot += n3
-if (n4 % 2) == 0: tot += n4
-if (n5 % 2) == 0: tot += n5
-if (n6 % 2) == 0: tot += n6
-
+for c in range(0,6):
+    n = int(input('Informe o {}º número: '.format(int(c)+1)))
+    if (n % 2) == 0: tot += n
 print('Soma: {}'.format(tot))
