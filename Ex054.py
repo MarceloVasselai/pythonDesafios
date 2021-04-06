11b628b0d346fcc680a84bada84f51d45178339a
MarceloVasselai <mvasselai@gmail.com>
2021-04-06 11:27:40 -0300

    Laços For - Maior ou Menor de idade

--- Ex054.py
+++ Ex054.py
@@ -1,9 +1,9 @@
 for c in range(0,7):
-    a1 = int(input('Informe o {} ano: '.format(int(c)+1)))
-    if (2021-a1) <21:
-        print('Menor de Idade')
+    ano = int(input('Informe o ano de nascimento da {}ª pessoa: '.format(c+1)))
+    if (2021-ano) < 21:
+        print('A {}ª pessoa é Menor de Idade'.format(c+1))
     else:
-        print('Maior de Idade')
+        print('A {}ª pessoa é Maior de Idade'.format(c+1))
 
 
 
