f45b759b3d7a07ac804ba6b48ea4159d1cbb6e19
MarceloVasselai <mvasselai@gmail.com>
2021-04-11 14:27:41 -0300

    Laços For - Razão

---
+++ Ex051.py
@@ -0,0 +1,6 @@
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10-1) * razao
+for c in range(primeiro,decimo + razao,razao):
+    print('{} '.format(c),end='-> ')
+print ('Acabou')
