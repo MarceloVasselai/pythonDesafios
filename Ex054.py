cf587f25dd556412ab5fe81ebea8158ab083d92e
MarceloVasselai <mvasselai@gmail.com>
2021-04-11 14:11:39 -0300

    Laços For - Maior ou Menor de Idade

--- Ex054.py
+++ Ex054.py
@@ -1,9 +1,20 @@
-for c in range(0,7):
-    ano = int(input('Informe o ano de nascimento da {}ª pessoa: '.format(c+1)))
-    if (2021-ano) < 21:
-        print('A {}ª pessoa é Menor de Idade'.format(c+1))
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+
+for c in range(1,9):
+
+    nasc = int(input( 'Em que a ano a {}ª pessoa nasceu: '.format(c)))
+    idade = atual - nasc
+
+    if idade >= 21:
+        print('A {}ª pessoa é Maior de Idade'.format(c))
+        totmaior += 1
     else:
-        print('A {}ª pessoa é Maior de Idade'.format(c+1))
+        print('A {}ª pessoa é Menor de Idade'.format(c))
+        totmenor += 1
 
-
+print('Total de {} pessoas maior de idade'.format(totmaior))
+print('Total de {} pessoas menor de idade'.format(totmenor))
 
