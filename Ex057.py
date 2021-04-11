051d990848d0b57a48eb8200746be2713a439c62
MarceloVasselai <mvasselai@gmail.com>
2021-04-11 15:15:29 -0300

    Laços While - Gênero

--- Ex057.py
+++ Ex057.py
@@ -1,4 +1,5 @@
-sexo = '0'
-while sexo not in ['F','M']:
-    sexo = input('Digite o Sexo: ')
-print('FIM')
+genero = str(input('Digite o Sexo (M/F): ')).strip().upper()[0]
+
+while genero not in ['M','F']:
+    genero = str(input("Dados inválidos, favor informar o Gênero: ")).strip().upper()[0]
+print('Gênero {} registrado com sucesso'.format(genero))
