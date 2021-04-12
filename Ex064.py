7841583ef60e6cbbd519b435ea6fe598643e6e53
MarceloVasselai <mvasselai@gmail.com>
2021-04-12 15:22:29 -0300

    Laços While - Soma e Contagem de Números

---
+++ Ex064.py
@@ -0,0 +1,10 @@
+n=0
+cont = 0
+soma = 0
+while n != 999:
+    n = int(input('Informe um número [999 p/ sair]: '))
+    if n != 999:
+        soma += n
+        cont += 1
+print('A soma foi {}'.format(soma))
+print('Total de {} números',format(cont))
