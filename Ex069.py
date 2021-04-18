fb6e6134c20d014a7a9822f6d46e68cf2e1e1570
MarceloVasselai <mvasselai@gmail.com>
2021-04-18 15:37:42 -0300

    Laços While - Gênero e Idade

--- Ex069.py
+++ Ex069.py
@@ -1,23 +1,18 @@
-continuar = 'S'
-p18 = 0
-homens = 0
-mulheres = 0
-
-while continuar == 'S':
-
+p18 = homens = mulheres = 0
+while True:
     idade = int(input('Informe a idade: '))
-    sexo = str(input('Qual o gênero [M/F] ?:  ')).upper()
-
-    if idade > 18:
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Qual o gênero [M/F] ?:  ')).strip().upper()[0]
+    if idade >= 18:
         p18 += 1
     if sexo == 'M':
         homens += 1
-    elif sexo == 'F' and idade < 20:
+    if sexo == 'F' and idade < 20:
         mulheres += 1
-    else:
-        print('Parâmetros inválidos')
-
-    continuar = str(input('Deseja continuar [S/N] ?:  ')).upper()
-
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Deseja continuar [S/N] ?:  ')).strip().upper()[0]
     if continuar == 'N':
-        print('Foram {} pessoas com mais de 18 anos, foram {} homens cadastrados e {} mulher com menos de 20 anos'.format(p18,homens,mulheres))
+        break
+print('Foram {} pessoas com mais de 18 anos, foram {} homens cadastrados e {} mulher com menos de 20 anos'.format(p18,homens,mulheres))
