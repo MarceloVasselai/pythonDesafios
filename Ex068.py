9e6dbb991bd29e3bf90eb932b4ed222f1a8a5896
MarceloVasselai <mvasselai@gmail.com>
2021-04-18 14:54:48 -0300

    Laços While - Par ou Ímpar

--- Ex068.py
+++ Ex068.py
@@ -1,22 +1,29 @@
-import random
+from random import randint
+vitorias=0
+while True:
 
-resultado = 'VENCEU'
+    p1 = int(input('Digite um número: '))
+    comp = randint(0,10)
+    soma = p1 + comp
+    pi = ' '
+    while pi not in 'PI':
+        pi = str(input('Par ou Ímpar? [P/I]: ')).strip().upper()[0]
+    print (f'Você escolheu {p1} e o Computador {comp}. O Resultado foi {soma} ',end='')
+    print ('e deu PAR' if soma % 2 == 0 else 'e deu Ímpar')
 
-while resultado != 'PERDER':
-
-    n1 = int(input('Digite um número: '))
-    pi = str(input('Par ou Ímpar? [P/I]: ')).upper()
-    n2 = random.randint(0,10)
-    soma = n1 + n2
-    if (soma % 2) == 0 and pi == 'I':
-        print('Você Perdeu !!! Informou o número {} e o Computador {}, totalizando {}'.format(n1,n2,n1+n2))
-        resultado = 'PERDER'
-    elif (soma % 2) == 0 and pi == 'P':
-        print('Você Ganhou !!! Informou o número {} e o Computador {}, totalizando {}'.format(n1,n2,n1+n2))
-    elif (soma % 2) != 0 and pi == 'I':
-        print('Você Ganhou !!! Informou o número {} e o Computador {}, totalizando {}'.format(n1,n2,n1+n2))
-    elif (soma % 2) != 0 and pi == 'P':
-        print('Você Perdeu !!! Informou o número {} e o Computador {}, totalizando {}'.format(n1,n2,n1+n2))
-        resultado = 'PERDER'
-    else:
-        print('Parâmetros inválidos')
+    if pi == 'P':
+        if soma % 2 == 0:
+            print('Você Venceu')
+            vitorias += 1
+        else:
+            print('Você Perdeu')
+            break
+    elif pi == 'I':
+        if soma % 2 == 1:
+            print('Você Venceu')
+            vitorias += 1
+        else:
+            print('Você Perdeu')
+            break
+    print('Vamos tentar novamente ?')
+print(f'Fim do Par ou Ímpar! Ganhou por {vitorias} vez(es)')
