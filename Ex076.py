9ded959b51a8a7d1286a4b0c2c80692109193c9f
MarceloVasselai <mvasselai@gmail.com>
2021-04-20 18:42:42 -0300

    Tuplas - Lista de Preços

---
+++ Ex076.py
@@ -0,0 +1,19 @@
+lista = ('Caneta', 1.99,
+         'Borracha', 1.99,
+         'Apotador', 2,
+         'Régua', 3,
+         'Sulfite', 0.1,
+         'Compasso', 2.32,
+         'Transferidor', 111.99
+         )
+
+print ('-' * 50)
+print (f'{"LISTAGEM DE PREÇOS":^40}')
+print ('-' * 50)
+
+for pos in range(0,len(lista)):
+    if pos % 2 == 0:
+        print (f'{lista[pos]:.<30}', end='')
+    else:
+        print(f'R$ {lista[pos]:>7.2f}')
+print ('-' * 50)
