0c599c7e5624b12129d225ec8f843cadb3a43f58
MarceloVasselai <mvasselai@gmail.com>
2021-04-24 14:28:00 -0300

    Listas - Imprimir e ordenar a Lista

---
+++ Ex079.py
@@ -0,0 +1,13 @@
+num=list()
+while True:
+    n=int(input('Digite um Valor: '))
+    if n not in num:
+        num.append(n)
+        print('adicionado')
+    else:
+        print('já consta na lista')
+    r=str(input('quer continuar? [S/N] '))
+    if r in 'Nn':
+        break
+num.sort()
+print(f'Os números digitados foram: {num} ')
