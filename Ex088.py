e817862a2a7b6d7b6e9ead53c0c6d31a456171b1
MarceloVasselai <mvasselai@gmail.com>
2021-04-29 18:18:15 -0300

    Lista Composta - Mega-Sena

--- Ex088.py
+++ Ex088.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep
 lista = list()
 jogos = list()
-print('$_' * 10,'MEGA SENA','$_' * 10)
+print('$_' * 10, 'MEGA SENA', '$_' * 10)
 qtde = int(input('Quantos jogos você quer que eu sorteie: '))
 tot = 1
 while tot <= qtde:
@@ -18,8 +18,8 @@ while tot <= qtde:
     jogos.append(lista[:])
     lista.clear()
     tot += 1
-print('$_' * 10,f'SORTEANDO {qtde} Jogos','$_'* 10)
-for i,l in enumerate(jogos):
+print('$_' * 10, f'SORTEANDO {qtde} Jogos', '$_'* 10)
+for i, l in enumerate(jogos):
     print(f'Jogo {i+1}:{l}')
     sleep(1)
-print('$_' * 10,'BOA SORTE','$_' * 10)
+print('$_' * 10, 'BOA SORTE', '$_' * 10)
