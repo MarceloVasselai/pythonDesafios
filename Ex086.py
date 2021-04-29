0cad1cda7b3bff08bed11b0c553016fbefbfc5a1
MarceloVasselai <mvasselai@gmail.com>
2021-04-29 18:14:54 -0300

    Lista Composta - Matriz

--- Ex086.py
+++ Ex086.py
@@ -5,5 +5,5 @@ for l in range(0,3):
 print('+-'*30)
 for l in range(0,3):
     for c in range(0, 3):
-        print(f'[{matriz[l][c]:^5}]',end='')
+        print(f'[{matriz[l][c]:^5}]', end='')
     print()
